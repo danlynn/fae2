29730df622ef2456a3822a6cb357e067f71c4fd3
Dan Lynn <git@danlynn.org>
2015-12-21 14:37:33 -0500

    Switch over to use sqlite3

--- fae2/fae2/base.py
+++ fae2/fae2/base.py
@@ -115,7 +115,8 @@ WSGI_APPLICATION = 'fae2.wsgi.application'
 
 DATABASES = {
     'default': {
-          'ENGINE' : 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+          # 'ENGINE' : 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+          'ENGINE' : 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
             'NAME' : get_secret('DATABASE_NAME'),              # Or path to database file if using sqlite3.
             'USER' : get_secret('DATABASE_USER'),              # Not used with sqlite3.
         'PASSWORD' : get_secret('DATABASE_PASSWORD'),          # Not used with sqlite3.
